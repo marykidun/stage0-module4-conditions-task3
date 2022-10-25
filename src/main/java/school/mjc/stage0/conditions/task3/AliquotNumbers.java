fa40da804dbf27a84fb34ab80bd0032499fe9455
Maryna Kidun <marykidun@gmail.com>
2022-10-26 00:03:58 +0300

    needed to finish

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -1,6 +1,14 @@
 package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
-    public void isFirstAliquot(int first, int second) {
+    public static void isFirstAliquot(int first, int second) {
+        if (first >= 2 && first % second == 0)
+            System.out.println("Aliqout");
+        else
+            System.out.println("Not aliqout");
+    }
+
+    public static void main(String[] args) {
+        isFirstAliquot(6, 12);
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+
+
     }
 }
