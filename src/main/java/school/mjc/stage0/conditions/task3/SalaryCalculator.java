6b47cdb1e3060e2ba4ab0d3a171fe6197cd7dd29
Maryna Kidun <marykidun@gmail.com>
2022-10-26 23:09:54 +0300

    needed to finish

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -1,8 +1,23 @@
 package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
-    public void calculateSalary(int salary) {
+    public static void calculateSalary(int salary) {
 
 
+        if (salary <= 10000 && salary > 0) {
+            System.out.println(salary - (salary * 0.15));
+        }
+        else if (salary > 10000 && salary <= 20000) {
+            System.out.println(salary - (salary * 0.18));
+        }
+        else if (salary > 20000) {
+            System.out.println(salary - (salary * 0.20));
+        }
+        else {
+            System.out.println("wrong input!");
+        }
+    }
+    public static void main(String[] args) {
+        calculateSalary(70000);
     }
 }
